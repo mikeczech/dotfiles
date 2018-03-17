0776db495d45dbdc6bc15aff0cfc1a21ef648343
Mike Czech <mail@mczech.de>
2018-03-17 20:56:40 +0100

    Add path to ruby gem

--- config.fish
+++ config.fish
@@ -4,6 +4,8 @@ export LANG=en_US.UTF-8
 export VISUAL=vim
 export EDITOR="$VISUAL"
 
+set -gx PATH ~/.gem/ruby/2.5.0/bin $PATH
+
 # for Yubikey SSH
 export SSH_AUTH_SOCK=/run/user/(id -u)/gnupg/S.gpg-agent.ssh
 gpg-connect-agent updatestartuptty /bye
